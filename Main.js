287e622c28c5c81ad3acd28a661678d81de4105a
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2022-03-27 00:49:31 -0400

    added functions to drop the tetra and recognize a game over

--- Main.js
+++ Main.js
@@ -2,11 +2,7 @@
 //Nested for loop, outer layer creates the row, inner loop fills the row with the tiles
 //All generated tiles get added to a generated tiles array
 //Grid will then be appended to an existing div
-
 let generatedTiles = [];
-let filledTiles = [];
-let tetraminoPosition;
-let movementAllowed = false;
 
 function generateGrid(width, height){
     let output = document.createElement('div');
@@ -34,7 +30,6 @@ function generateGrid(width, height){
 
 //tile position represented by respective tile having a grey background color of #A9A9A9
 //function returns current tile position to white and updates current position and logs the change
-
 function updateTetraminoPosition(newPosition) {
     document.getElementById(tetraminoPosition).style.backgroundColor = white;
     document.getElementById(newPosition).style.backgroundColor = '#A9A9A9';
@@ -44,7 +39,10 @@ function updateTetraminoPosition(newPosition) {
 
 //global variable movementAllowed disables / enables movement of the tile
 //function move tile checks if the proposed position is filled by another tile or out of bounds, only accepts a s d as inputs
-
+//returns the new tile
+let movementAllowed = false;
+let filledTiles = [];
+let tetraminoPosition = '49';
 function moveTile(movementInput) {
     if(movementAllowed) {
 
@@ -75,12 +73,11 @@ function moveTile(movementInput) {
             
             //checking if proposed position is out of bounds OR filled
             if(proposedXCoord < 1 || proposedXCoord > 7 || proposedYCoord < 1 || filledTiles.includes(proposedPosition)) {
-                filledTiles.push(proposedPosition);
                 console.log(`proposed position ${proposedPostion} is out of bounds or filled`);
                 return 'movement blocked';
             } else {
                 updateTetraminoPosition(proposedPosition);
-                return 'tile moved';
+                return proposedPosition;
             }
 
         } else {
@@ -92,4 +89,45 @@ function moveTile(movementInput) {
     }
 }
 
+
+//function to move the tetra down, filling the position if the movement fails (function returns 'movement blocked')
+//ends the game if the tile stops at position 49
+function dropTile() {
+    let newPosition = moveTile('s');
+    if(newPosition = 'movement Blocked'){
+        filledTiles.push(tetraminoPosition);
+        updateTetraminoPosition('49');
+    }
+    if(filledTiles.includes('49')){
+        movementAllowed = false;
+        fallingState(false);
+        document.getElementById('gameOverScreen').style.display = 'absolute';
+    }
+}
+
+
+
+//function to start/stop the tile falling
+//uses a global variable to store the state
+let fallingTetra;
+function fallingState(inputBool) {
+    if(inputBool) {
+        fallingTetra = setInterval(dropTetra, 500);
+    } else {
+        clearInterval(fallingTetra);
+    }
+}
+
+//function resets tile position to 49, clears all the tile backgrounds back to white, hides the game over / game title screen and starts the tile falling
+function startGame() {
+    filledTiles.forEach((x) => {document.getElementById(x).style.backgroundColor = white});
+    updateTetraminoPosition('49');
+    console.log(`startGame() triggered`)
+    document.getElementById(`gameOverScreen`).style.display = "none";
+    document.getElementById(`gameTitleScreen`).style.display = "none";
+    fallingState(true);
+    movementAllowed = true;
+}
+
 document.getElementById('playingGrid').appendChild(generateGrid(7,9));
+
